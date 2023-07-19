51bfe2623de90ca6ca8d67edce9952d8abfd2d2b
Divya140697 <125757414+Divya140697@users.noreply.github.com>
2023-07-19 14:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your code here
+// script.js
+const items = document.querySelectorAll('.item');
+
+let draggedItem = null;
+
+function handleDragStart() {
+  draggedItem = this;
+  this.classList.add('dragging');
+}
+
+function handleDragEnd() {
+  this.classList.remove('dragging');
+}
+
+function handleDragOver(e) {
+  e.preventDefault();
+}
+
+function handleDragEnter() {
+  this.classList.add('over');
+}
+
+function handleDragLeave() {
+  this.classList.remove('over');
+}
+
+function handleDrop() {
+  if (draggedItem !== this) {
+    // Swap the background images
+    const tempBg = this.style.backgroundImage;
+    this.style.backgroundImage = draggedItem.style.backgroundImage;
+    draggedItem.style.backgroundImage = tempBg;
+  }
+}
+
+items.forEach(item => {
+  item.addEventListener('dragstart', handleDragStart);
+  item.addEventListener('dragend', handleDragEnd);
+  item.addEventListener('dragover', handleDragOver);
+  item.addEventListener('dragenter', handleDragEnter);
+  item.addEventListener('dragleave', handleDragLeave);
+  item.addEventListener('drop', handleDrop);
+});
